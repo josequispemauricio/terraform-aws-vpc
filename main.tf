02d36e9f4b6803282f1a856af76dfb7dbaa1641c
josegabrielquispemauricio <jose.quispe@external.totalenergies.fr>
2024-03-30 16:43:07 +0100

    updatesource

--- main.tf
+++ main.tf
@@ -12,7 +12,8 @@ resource "aws_vpc" "vpc_module" {
 resource "aws_subnet" "subnet_modulevpc" {
   for_each = var.subnets_data_input
 
-  vpc_id = aws_vpc.vpc_module.id
-  cidr_block = each.value["cidr_block_x"]
+  vpc_id            = aws_vpc.vpc_module.id
+  cidr_block        = each.value["cidr_block_x"]
   availability_zone = each.value["availability_zone_test"]
+  //availability_zone = data.aws_availability_zones.availability_zones.names[0]
 }
